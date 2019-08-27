afb4db44ed3ffee0440ab6fe6498bb907d141445
zouyouxiu <1842036736@qq.com>
2019-08-27 15:09:05 +0800

    打war包无法运行问题解决

--- consumer/src/main/java/com/tangxian/ConsumerApplication.java
+++ consumer/src/main/java/com/tangxian/ConsumerApplication.java
@@ -7,14 +7,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = WebAutoConfiguration.class)
 @EnableDubboConfiguration
-public class ConsumerApplication {
-	private final static Logger logger = LoggerFactory.getLogger(ConsumerApplication.class);
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-		logger.info("ConsumerApplication is success Dubbo consumer启动成功!");
-	}
+public class ConsumerApplication extends SpringBootServletInitializer {
+    private final static Logger logger = LoggerFactory.getLogger(ConsumerApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+        logger.info("ConsumerApplication is success Dubbo consumer启动成功!");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(ConsumerApplication.class);
+    }
+
 }
